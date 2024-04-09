fa47a39c365fe10bb876779c3a7d3121b992e823
Gavin Chock-Chiong <gavin.chockchiong@gmail.com>
2024-04-09 13:29:26 -0400

    showing main menu with all the methods we need to code

--- Driver.java
+++ Driver.java
@@ -32,8 +32,8 @@ public class Driver {
                         String word = sc.nextLine();
                          
                         // if the line is empty, it is the end of the block of words
-                        if (word.equals(""))
-                            break;
+                        if (word.equals("")){
+                            break;}
 
                         WordList.addWord(word);
                     }
@@ -51,4 +51,70 @@ public class Driver {
             System.out.println("Error: " + e.getMessage());
         }
     }
+
+public static void mainMenu(){
+        System.out.println("1 browse a topic");
+        System.out.println("2 insert a new topic before another one");
+        System.out.println("3 insert a new topic after another one");
+        System.out.println("4 remove a topic");
+        System.out.println("5 modify a topic");
+        System.out.println("6 search topics for a word");
+        System.out.println("7 load from a file");
+        System.out.println("8 show all words starting with a given letter");
+        System.out.println("9 save to file");
+        System.out.println("0 exit");
+        System.out.println("-----------------------------");
+
+        Scanner in = new Scanner(System.in);
+        int choice = in.nextInt();
+
+        switch (choice){
+
+            case 1:
+                browseTopic();
+                break;
+
+            case 2:
+                insertTopicBefore();
+                break;
+
+            case 3:
+                insertTopicAfter();
+                break;
+
+            case 4:
+                removeTopic();
+                break;
+
+            case 5:
+                modifyTopic();
+                break;
+
+            case 6:
+                searchWord();
+                break;
+
+            case 7:
+                loadFile();
+                break;
+            
+            case 8:
+                showWords();
+                break;
+
+            case 9:
+                saveFile();
+                break; 
+
+            case 0:
+                System.exit(0);
+                break;
+
+            default:
+                System.out.println("Invalid choice");
+                mainMenu();
+                break;
+        }
+}
+
 }
