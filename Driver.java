6e6a6b9426c9a26ad21b97bf85ad4b48d13d62ad
AlexLuangxay <alexluangxay@gmail.com>
2024-04-09 14:27:36 -0400

    function print pickATopic done

--- Driver.java
+++ Driver.java
@@ -55,72 +55,70 @@ public class Driver {
         return vocabList;
     }
 
-    /*
-     * public static void mainMenu() {
-     * System.out.println("1 browse a topic");
-     * System.out.println("2 insert a new topic before another one");
-     * System.out.println("3 insert a new topic after another one");
-     * System.out.println("4 remove a topic");
-     * System.out.println("5 modify a topic");
-     * System.out.println("6 search topics for a word");
-     * System.out.println("7 load from a file");
-     * System.out.println("8 show all words starting with a given letter");
-     * System.out.println("9 save to file");
-     * System.out.println("0 exit");
-     * System.out.println("-----------------------------");
-     * 
-     * Scanner in = new Scanner(System.in);
-     * int choice = in.nextInt();
-     * 
-     * switch (choice) {
-     * 
-     * case 1:
-     * browseTopic();
-     * break;
-     * 
-     * case 2:
-     * insertTopicBefore();
-     * break;
-     * 
-     * case 3:
-     * insertTopicAfter();
-     * break;
-     * 
-     * case 4:
-     * removeTopic();
-     * break;
-     * 
-     * case 5:
-     * modifyTopic();
-     * break;
-     * 
-     * case 6:
-     * searchWord();
-     * break;
-     * 
-     * case 7:
-     * loadFile();
-     * break;
-     * 
-     * case 8:
-     * showWords();
-     * break;
-     * 
-     * case 9:
-     * saveFile();
-     * break;
-     * 
-     * case 0:
-     * System.exit(0);
-     * break;
-     * 
-     * default:
-     * System.out.println("Invalid choice");
-     * mainMenu();
-     * break;
-     * }
-     * }
-     */
+    public static void mainMenu() {
+        System.out.println("1 browse a topic");
+        System.out.println("2 insert a new topic before another one");
+        System.out.println("3 insert a new topic after another one");
+        System.out.println("4 remove a topic");
+        System.out.println("5 modify a topic");
+        System.out.println("6 search topics for a word");
+        System.out.println("7 load from a file");
+        System.out.println("8 show all words starting with a given letter");
+        System.out.println("9 save to file");
+        System.out.println("0 exit");
+        System.out.println("-----------------------------");
+
+        Scanner in = new Scanner(System.in);
+        int choice = in.nextInt();
+
+        switch (choice) {
+
+            case 1:
+                browseTopic();
+                break;
+
+            case 2:
+                insertTopicBefore();
+                break;
+
+            case 3:
+                insertTopicAfter();
+                break;
+
+            case 4:
+                removeTopic();
+                break;
+
+            case 5:
+                modifyTopic();
+                break;
+
+            case 6:
+                searchWord();
+                break;
+
+            case 7:
+                loadFile();
+                break;
+
+            case 8:
+                showWords();
+                break;
+
+            case 9:
+                saveFile();
+                break;
+
+            case 0:
+                System.exit(0);
+                break;
+
+            default:
+                System.out.println("Invalid choice");
+                mainMenu();
+                break;
+        }
+    }
 
     public static void printPickATopic(VocabList vocabList) {
         System.out.println("Pick a topic");
